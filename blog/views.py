662b7fae2969bd7b7d152a8889ec6768d8556ad3
alsdn82 <vhrcnsehr@naver.com>
2021-10-20 13:20:35 +0900

    blog

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from blog.models import Post
+
+
+def index(request):
+    post_list = Post.objects.all()
+    context = {'post_list': post_list}
+    return render(request, 'blog/index.html', context)
+    #return HttpResponse("안녕하세요~ 블로그 페이지입니다.")
